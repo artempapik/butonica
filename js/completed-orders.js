d6dd685f5b3b9748ec7ff7be4c52f0b0823651a5
artempapik <51436664+artempapik@users.noreply.github.com>
2023-10-24 13:49:41 +0300

    Add files via upload

---
+++ js/completed-orders.js
@@ -0,0 +1,19 @@
+let completedOrdersTable
+
+const showCompletedOrderInfo = e => {
+    fillSelectedMenuItem(e)
+    main.innerHTML = menuItemsContents['completedorder']
+    completedOrdersTable = document.querySelector('.completed-order-table table')
+
+    get(`Order/completed/${loginInfo.companyId}`).then(response => {
+        if (response.length) {
+            completedOrdersTable.style.display = 'block'
+        }
+
+        response.forEach(o => fillCompletedOrdersTable(o))
+    }).catch(() => showMessage('error', getErrorMessage(ORDER)))
+
+    get(`Product/ids-names/${loginInfo.companyId}`).then(response => orderProducts = response)
+}
+
+const fillCompletedOrdersTable = order => completedOrdersTable.append(createOrderRow(order, completedOrdersTable))
