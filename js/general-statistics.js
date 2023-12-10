0e6c58f564fba8300d5d49c107b77189d8873bf4
artempapik <51436664+artempapik@users.noreply.github.com>
2023-12-10 20:29:23 +0200

    Update general-statistics.js

--- js/general-statistics.js
+++ js/general-statistics.js
@@ -1,15 +1,18 @@
+const getStatisticsValues = month => get(`Statistics/general/${loginInfo.companyId}/${month || new Date().getMonth() + 1}`).then(response => {
+    const statValues = document.querySelectorAll('.general-statistics-info .stat-value span:first-child')
+    statValues.item(0).parentNode.classList = `stat-value ${getClassForNumber(response[0])}`
+
+    for (let i = 0; i < statValues.length; i++) {
+        statValues.item(i).textContent = (+response[i]).toFixed(2)
+    }
+
+    replaceLoadIcons()
+}).catch(() => showMessage('error', getErrorMessage('статистику')))
+
 const showGeneralStatisticsInfo = e => {
     fillSelectedMenuItem(e)
     main.innerHTML = menuItemsContents['generalstatistics']
-
-    get(`Statistics/general/${loginInfo.companyId}`).then(response => {
-        const statValues = document.querySelectorAll('.general-statistics-info .stat-value span:first-child')
-        statValues.item(0).parentNode.classList.add(getClassForNumber(response[0]))
-
-        for (let i = 0; i < statValues.length; i++) {
-            statValues.item(i).textContent = (+response[i]).toFixed(2)
-        }
-
-        replaceLoadIcons()
-    }).catch(() => showMessage('error', getErrorMessage('статистику')))
+    getStatisticsValues(0)
 }
+
+const changeStatisticsMonth = e => getStatisticsValues(e.target.selectedIndex)
