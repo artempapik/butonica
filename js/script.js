74dbdb9ccb1b7cb713ab103c42dae1c764fb1140
artempapik <51436664+artempapik@users.noreply.github.com>
2023-12-30 19:12:48 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1049,6 +1049,7 @@ const fillSelectedMenuItem = e => {
     currentPage = 1
     pressMenuButton()
 
+    document.body.style.height = 'fit-content'
     document.querySelector('header').style.display = ''
     main.style.background = 'unset'
     main.classList.remove('sale-padding')
