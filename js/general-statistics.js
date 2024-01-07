23f12e97c2bbeb1b7d15618b1c1f645e274d1f90
artempapik <51436664+artempapik@users.noreply.github.com>
2024-01-07 14:56:23 +0200

    Add files via upload

---
+++ js/general-statistics.js
@@ -0,0 +1,20 @@
+const getStatisticsValues = month => {
+    showLoadAnimation()
+
+    get(`Statistics/general/${loginInfo.companyId}/${month || new Date().getMonth() + 1}`).then(response => {
+        const statValues = document.querySelectorAll('.general-statistics-info .stat-value span:first-child')
+        statValues.item(0).parentNode.classList = `stat-value ${getClassForNumber(response[0])}`
+
+        for (let i = 0; i < statValues.length; i++) {
+            statValues.item(i).textContent = (+response[i]).toFixed(2)
+        }
+
+        replaceLoadIcons()
+    }).catch(() => showMessage('error', getErrorMessage('статистику')))
+}
+
+const showGeneralStatisticsInfo = e => {
+    fillSelectedMenuItem(e)
+    main.innerHTML = menuItemsContents['generalstatistics']
+    getStatisticsValues(0)
+}
