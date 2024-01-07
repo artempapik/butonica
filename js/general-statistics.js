e704fcaba9af0bbbef28b4812ae81f659466bbd2
artempapik <51436664+artempapik@users.noreply.github.com>
2024-01-07 23:02:47 +0200

    Update general-statistics.js

--- js/general-statistics.js
+++ js/general-statistics.js
@@ -1,20 +1,77 @@
+let expensesPieChart, incomePieChart, incomeByLabelPieChart, expensesIncomePieChart
+
 const getStatisticsValues = month => {
     showLoadAnimation()
 
-    get(`Statistics/general/${loginInfo.companyId}/${month || new Date().getMonth() + 1}`).then(response => {
+    if (month) {
+        month--
+    }
+
+    get(`Statistics/general/${loginInfo.companyId}/month/${month || new Date().getMonth() + 1}`).then(response => {
         const statValues = document.querySelectorAll('.general-statistics-info .stat-value span:first-child')
-        statValues.item(0).parentNode.classList = `stat-value ${getClassForNumber(response[0])}`
+        const generalNumbers = response.generalNumbers
+
+        statValues.item(0).parentNode.classList = `stat-value ${getClassForNumber(generalNumbers[0])}`
 
         for (let i = 0; i < statValues.length; i++) {
-            statValues.item(i).textContent = (+response[i]).toFixed(2)
+            statValues.item(i).textContent = (+generalNumbers[i]).toFixed(2)
         }
+
+        expensesPieChart.data.datasets[0].data = [generalNumbers[4], generalNumbers[5]]
+        expensesPieChart.update()
+
+        incomePieChart.data.datasets[0].data = [generalNumbers[2], generalNumbers[3]]
+        incomePieChart.update()
+
+        incomeByLabelPieChart.data.labels = response.internetOrdersIncomeByLabel.map(o => o.labelName || 'Без мітки')
+        incomeByLabelPieChart.data.datasets[0].data = response.internetOrdersIncomeByLabel.map(o => o.totalSum)
+        incomeByLabelPieChart.update()
+
+        expensesIncomePieChart.data.datasets[0].data = [generalNumbers[1], generalNumbers[4] + generalNumbers[5]]
+        expensesIncomePieChart.update()
 
         replaceLoadIcons()
     }).catch(() => showMessage('error', getErrorMessage('статистику')))
 }
 
+const getPieChart = (selector, ...labels) => new Chart(document.querySelector(`#${selector}-pie-chart`), {
+    type: 'pie',
+    data: {
+        labels,
+        datasets: [
+        {
+            data: new Array(labels.length || 10).fill(0),
+            borderWidth: 2,
+        }]
+    },
+    options: {
+        plugins: {
+            legend: {
+                position: 'bottom',
+                align: 'start'
+            },
+            tooltip: {
+                enabled: true,
+                callbacks: {
+                    footer: tooltip => `${(tooltip[0].parsed * 100 / tooltip[0].dataset.data.reduce((total, current) => total + current, 0)).toFixed(2)}%`
+                }
+            }
+        },
+        scales: {
+            y: {
+                display: false,
+            }
+        }
+    }
+})
+
 const showGeneralStatisticsInfo = e => {
     fillSelectedMenuItem(e)
     main.innerHTML = menuItemsContents['generalstatistics']
     getStatisticsValues(0)
+
+    expensesPieChart = getPieChart('expenses', 'витрати магазину', 'витрати на товар')
+    incomePieChart = getPieChart('income', 'продажі', 'інтернет-замовлення')
+    incomeByLabelPieChart = getPieChart('income-by-label')
+    expensesIncomePieChart = getPieChart('expenses-income', 'доходи', 'витрати')
 }
