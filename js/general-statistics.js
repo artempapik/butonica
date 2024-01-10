62e12de4bb7a890e24d38b70f16d66dbfd7acc73
artempapik <51436664+artempapik@users.noreply.github.com>
2024-01-10 23:13:54 +0200

    Update general-statistics.js

--- js/general-statistics.js
+++ js/general-statistics.js
@@ -339,10 +339,17 @@ const updateChartsFontSize = () => {
     setFontSize(pieCharts, 16)
     setFontSize(barCharts, 24)
 
-    if (window.innerWidth <= 1400) {
-        setFontSize(pieCharts, 24)
-        setFontSize(barCharts, 20)
-        barCharts.forEach(c => c.options.layout.padding = null)
+    if (isMobile) {
+        if (window.innerWidth <= 900) {
+            setFontSize(pieCharts, 24)
+            setFontSize(barCharts, 20)
+        }
+
+        if (window.innerWidth <= 1400) {
+            setFontSize(pieCharts, 32)
+            setFontSize(barCharts, 28)
+            barCharts.forEach(c => c.options.layout.padding = null)
+        }   
     }
 
     pieCharts.forEach(c => c.update())
