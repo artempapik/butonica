45f9bad630d0cd3d5c7c28ac16cb6c64ba073450
artempapik <51436664+artempapik@users.noreply.github.com>
2024-01-11 16:08:50 +0200

    Update general-statistics.js

--- js/general-statistics.js
+++ js/general-statistics.js
@@ -280,7 +280,7 @@ const getPieChart = (selector, title, isTooltipEnabled, ...labels) => new Chart(
                 font: {
                     family: "monospace, 'SF Mono', Roboto",
                     weight: 'bold',
-                    size: !isMobile && window.innerWidth <= 1500 ? 16 : 14
+                    size: !isMobile && window.innerWidth <= 1500 ? 15 : 14
                 },
                 formatter: value => value.toFixed(0) + (isMobile || (!isMobile && window.innerWidth <= 1500) ? '' : ' грн'),
                 color: 'rgb(240, 240, 240)'
