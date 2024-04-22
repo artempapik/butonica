acecd3b3d1068ad7914777b845134327adce14c5
artempapik <51436664+artempapik@users.noreply.github.com>
2024-04-22 17:50:37 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2161,42 +2161,6 @@ const select2NoResults = {
     }
 }
 
-function createHandler(func, timeout) {
-
-    let timer = null;
-    let pressed = false;
-  
-    return function() {
-  
-      if (timer) {
-        clearTimeout(timer);
-      }
-  
-      if (pressed) {
-        if (func) {
-          func.apply(this, arguments);
-        }
-        clear();
-      } else {
-        pressed = true;
-        setTimeout(clear, timeout || 500);
-      }
-  
-    };
-  
-    function clear() {
-      timeout = null;
-      pressed = false;
-    }
-  
-  }
-  
-  // ....
-  // And you would use it like this:
-  
-  const ignore = createHandler((e) => e.preventDefault(), 500);
-  document.body.addEventListener('touchstart', ignore, { passive: false });
-
 // Notification.requestPermission().then(permission => console.log(permission))
 
 // navigator.serviceWorker.ready.then(registration => registration.pushManager.subscribe({
