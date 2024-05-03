6d4c42231624ac96b1d4d7cc9b4017f673e90219
artempapik <51436664+artempapik@users.noreply.github.com>
2024-05-04 01:46:02 +0300

    Update changelog.js

--- js/changelog.js
+++ js/changelog.js
@@ -12,7 +12,7 @@ const changelogs = [
         changes: [
             {
                 type: CHANGELOG_TYPES.fire,
-                text: 'тепер при створенні та редагуванні товарів можна вписувати їм закупівельну ціну'
+                text: 'тепер при створенні та редагуванні товарів ви можете змінити закупівельну ціну'
             },
             {
                 type: CHANGELOG_TYPES.exclamation,
