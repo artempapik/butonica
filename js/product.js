7b4de59d72d7f9ec000a798783b95986817a7bed
artempapik <51436664+artempapik@users.noreply.github.com>
2024-05-30 11:49:56 +0300

    Update product.js

--- js/product.js
+++ js/product.js
@@ -32,7 +32,7 @@ const showProductInfo = e => {
         }
 
         products.forEach(p => fillProductsTable(p))
-        setFixedTable(productsTable, [2])
+        setFixedTable(productsTable, [1, 2])
         replaceLoadIcons()
     }).catch(() => showMessage('error', getErrorMessage('товари')))
 
